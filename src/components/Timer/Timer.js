f70a3aaebaa1c343448b64c72b72383c9d091aee
Hasst <sergey1112345@gmail.com>
2020-12-18 21:41:32 +0200

    added changing minutes

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -4,14 +4,30 @@ import './Timer.css';
 import btnCloseSvg from '../../assets/svg/btnCloseSvg';
 
 const Timer = () => {
-  const initialState = {};
+  const initialState = {isPlay: false, minutes: 23, seconds: 23};
   const [timer, setTimer] = useState(initialState);
+
+  const onChangeMinutes = (e) => {
+    let value = e.currentTarget.value;
+    value = value.match(/[\d]{0,2}/);
+    return setTimer((state) => {
+      return {...state, minutes: value};
+    });
+  };
 
   return (
     <div className="timer">
-      23:23:23
+      <div className="timer__minutes">
+        <input
+          contentEditable={!timer.isPlay}
+          suppressContentEditableWarning={true}
+          onInput={onChangeMinutes}
+          value={timer.minutes}
+        />
+      </div>
+      :<span className="timer__seconds">23</span>
       <div className="timer__btn-wrapper">
-        <button className="timer__close-btn btn">{btnCloseSvg}</button>
+        {timer.isPlay && <button className="timer__close-btn btn">{btnCloseSvg}</button>}
       </div>
     </div>
   );
