c55deb7dfdc50d831606e66577ef2acbbcd8c741
Hasst <sergey1112345@gmail.com>
2020-12-23 22:23:00 +0200

    added: input time

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -4,30 +4,53 @@ import './Timer.css';
 import btnCloseSvg from '../../assets/svg/btnCloseSvg';
 
 const Timer = () => {
-  const initialState = {isPlay: false, minutes: 23, seconds: 23};
+  const initialState = {inputTime: '', isPlay: false, minutes: 0, seconds: 0};
   const [timer, setTimer] = useState(initialState);
 
   const onChangeMinutes = (e) => {
-    let value = e.currentTarget.value;
-    value = value.match(/[\d]{0,2}/);
+    const value = e.currentTarget.value;
+    const receivedTime = value.match(/[\d]{0,4}/).toString();
+    let minutes = 0;
+    let seconds = 0;
+
+    if (receivedTime.length > 2) {
+      seconds = +receivedTime.slice(-2);
+      minutes = +receivedTime.slice(0, receivedTime.length / 2);
+    } else {
+      seconds = +receivedTime;
+    }
+
     return setTimer((state) => {
-      return {...state, minutes: value};
+      return {...state, inputTime: receivedTime, minutes, seconds};
     });
   };
 
+  const onChangeInputSelection = (e) => e.currentTarget.setSelectionRange(4, 4);
+
+  const fixTimeString = () => `${fixMinutesString()}м ${fixSecondsString()}с`;
+  const fixMinutesString = () => (timer.minutes < 10 ? `0${timer.minutes}` : `${timer.minutes}`);
+  const fixSecondsString = () => (timer.seconds < 10 ? `0${timer.seconds}` : `${timer.seconds}`);
+
   return (
     <div className="timer">
-      <div className="timer__minutes">
-        <input
-          contentEditable={!timer.isPlay}
-          suppressContentEditableWarning={true}
-          onInput={onChangeMinutes}
-          value={timer.minutes}
-        />
-      </div>
-      :<span className="timer__seconds">23</span>
+      {!timer.isPlay && (
+        <>
+          <input
+            className="timer__input-time"
+            onInput={onChangeMinutes}
+            value={timer.inputTime}
+            onClick={onChangeInputSelection}
+          />
+          <div className="timer__focus-border"></div>
+        </>
+      )}
+      <div className="time">{fixTimeString()}</div>
       <div className="timer__btn-wrapper">
-        {timer.isPlay && <button className="timer__close-btn btn">{btnCloseSvg}</button>}
+        {timer.isPlay ? (
+          <button className="timer__close-btn btn">{btnCloseSvg}</button>
+        ) : (
+          <button className="timer__close-btn btn">||</button>
+        )}
       </div>
     </div>
   );
